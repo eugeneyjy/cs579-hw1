e68c7a184be336f92b55411a5908ba115a6d9cc6
eugeneyjy <eugeneyjy314@gmail.com>
2023-04-13 16:15:21 -0700

    non code related commit

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python train.py --arch=lenet --batch-size=128 --epochs=10 --lr=0.003 --dataset=mnist
+python train.py --arch=lenet --batch-size=128 --epochs=30 --lr=0.0005 --dataset=mnist --optimizer=adam --no-save
